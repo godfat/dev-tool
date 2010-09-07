0cb4f2563f905f4c95a6affc98a52bd8b1395814
Lin Jen-Shin <godfat@godfat.org>
2010-09-07 23:07:05 +0800

    ok, i am tired of fish's buggy 256 color width calculation.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 
 function fish_prompt -d 'custom prompt'
-  echo \x1b'[38;5;102m'(date '+%H:%M')' '(set_color normal)(prompt-where)(set_color green)(prompt-cwd)(set_color cyan)(prompt-git)(set_color normal)'> '
+  echo (set_color white)(date '+%H:%M')' '(set_color normal)(prompt-where)(set_color green)(prompt-cwd)(set_color cyan)(prompt-git)(set_color normal)'> '
 end
