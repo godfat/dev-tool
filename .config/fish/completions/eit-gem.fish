435d0ec3c9996cca57d58847b953a3fd15be41f2
Lin Jen-Shin <godfat@godfat.org>
2012-04-20 17:30:00 +0800

    also auto-complete files in stdlib!

--- .config/fish/completions/eit-gem.fish
+++ .config/fish/completions/eit-gem.fish
@@ -1,2 +1,9 @@
 
+set __fish_eit_gem_path (brew --prefix ruby)/lib/ruby/1.9.1
+
+function __fish_eit_gem_sed
+  sed s/(echo $__fish_eit_gem_path | sed 's/\//\\\\\//g')\\///
+end
+
 complete -f -c eit-gem -a '(gem list | sed -E "s/ \(.+\)//")' -d 'Gem'
+complete -f -c eit-gem -a "(find $__fish_eit_gem_path -name '*.rb' | __fish_eit_gem_sed)" -d 'File'
