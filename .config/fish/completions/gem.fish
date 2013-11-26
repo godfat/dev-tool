4c2429a3cc539b5865a561b1c8e58dbdeaf9b39f
Lin Jen-Shin <godfat@godfat.org>
2013-11-27 06:44:19 +0800

    completion for `gem grep`

--- .config/fish/completions/gem.fish
+++ .config/fish/completions/gem.fish
@@ -9,9 +9,14 @@ function __fish_eit_gem_sed
 end
 
 complete -f -c gem -n '__fish_use_subcommand' -xa 'eit\t"'(_ "Edit a gem")'"'
+complete -f -c gem -n '__fish_use_subcommand' -xa 'grep\t"'(_ "Grep a gem")'"'
 
-complete -f -c gem -n 'contains eit (commandline -poc)' \
-         -a '(gem list | sed -E "s/ \(.+\)//")' -d 'Gem'
+set -l  gem_list -a '(gem list | sed -E "s/ \(.+\)//")' -d 'Gem'
+set -l file_list -a "(find $__fish_eit_gem_path -name '*.rb' 2> /dev/null \
+                  | __fish_eit_gem_sed)" -d 'File'
 
-complete -f -c gem -n 'contains eit (commandline -poc)' \
-         -a "(find $__fish_eit_gem_path -name '*.rb' 2> /dev/null | __fish_eit_gem_sed)" -d 'File'
+complete -f -c gem -n 'contains eit (commandline -poc)' $gem_list
+complete -f -c gem -n 'contains eit (commandline -poc)' $file_list
+
+complete -f -c gem -n 'contains grep (commandline -poc)' $gem_list
+complete -f -c gem -n 'contains grep (commandline -poc)' $file_list
