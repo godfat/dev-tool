21a991222cd80f96c7fc90191e2ad80bc6577869
Lin Jen-Shin <godfat@godfat.org>
2014-02-25 15:13:02 +0800

    looks like we no longer need those hacks for bundler!

--- .config/bundler/bundler/runtime.rb
+++
@@ -1,8 +0,0 @@
-
-require Gem.find_files('bundler/runtime')[1]
-
-class Bundler::Runtime
-  def setup_environment
-    # please don't mess up my environment with RUBYOPT, BUNDLE_GEMFILE, etc
-  end
-end
